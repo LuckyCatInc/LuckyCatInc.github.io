8db65b87ae992a15f7af457eda97bebfd026886b
appbana <howmuch515@gmail.com>
2024-09-23 01:30:42 +0900

    back-to-top ボタンを設置

--- docs/js/designed_script.js
+++ docs/js/designed_script.js
@@ -35,3 +35,13 @@ hamburger_panel_items.forEach(item => {
         }
     })
 })
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
